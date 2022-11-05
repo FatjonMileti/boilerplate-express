377b58e001d6fcff62dbe4f72a333023737170d8
Fatjon Mileti <91939874+FatjonMileti@users.noreply.github.com>
2022-11-05 10:18:10 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,13 @@
 let express = require('express');
 let app = express();
+//console.log("Hello World");
+//app.get("/", (req, res) => {
+//  res.send("Hello Express");
+//});
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 
+app.use('/public', express.static(__dirname + '/public'))
 
 
 
