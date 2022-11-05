bb261a2ed808afccea0367fb90ce5988d5f15d31
Fatjon Mileti <91939874+FatjonMileti@users.noreply.github.com>
2022-11-05 10:30:52 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -9,9 +9,16 @@ app.get("/", function(req, res) {
 
 app.use('/public', express.static(__dirname + '/public'))
 
-app.get("/json", (req, res) => {
-  res.json({
-    message: "Hello json"
+const mySecret = process.env['MESSAGE_STYLE'];
+
+app.get("/json",(req,res) => {
+ let response ;
+  if(process.env['MESSAGE_STYLE'] === "uppercase"){
+   response  = "Hello json".toUpperCase();
+  }else{
+    response  = "Hello json";
+  }
+  res.json({"message":`${response }`});
   });
 });
 
