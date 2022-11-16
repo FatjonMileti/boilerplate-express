2822d9e2777857d7680327ace6e37b314059839d
Fatjon Mileti <91939874+FatjonMileti@users.noreply.github.com>
2022-11-16 10:59:22 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,32 +1,74 @@
 let express = require('express');
 let app = express();
-//console.log("Hello World");
-//app.get("/", (req, res) => {
-//  res.send("Hello Express");
-//});
-app.get("/", function(req, res) {
+
+let bodyParser = require("body-parser")
+
+
+
+// #1 
+// console.log("Hello World")
+
+//#2 
+// app.get("/", (req, res) => {
+//   res.send("Hello Express")
+// })
+
+//#4
+app.use("/public", express.static(__dirname + "/public"))
+app.use(bodyParser.urlencoded({ extended: false }))
+
+//#7 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next()
+})
+
+// #3
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
+})
 
-app.use('/public', express.static(__dirname + '/public'))
+// #5
 
-const mySecret = process.env['MESSAGE_STYLE'];
+// app.get("/json", (req, res) => {
+//   res.json({"message": "Hello json"})
+// })
 
-app.get("/json",(req,res) => {
- let response ;
-  if(process.env['MESSAGE_STYLE'] === "uppercase"){
-   response  = "Hello json".toUpperCase();
-  }else{
-    response  = "Hello json";
+//#6 
+
+app.get("/json", (req, res) => {
+  if ( process.env["MESSAGE_STYLE"] == "uppercase"){
+    res.json({"message": "HELLO JSON"})
+  } else {
+      res.json({"message": "Hello json"})
   }
-  res.json({"message":`${response }`});
-  });
-});
+})
+
+//#8 
+app.get("/now", (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+}, (req, res) => {
+  res.json({"time": req.time})
+})
+
+//#9
+app.get("/:word/echo", (req, res) => {
+  res.json({ echo: req.params.word })
+})
 
 
+//#10 
 
+app.get("/name", (req, res) => {
+  res.json({ name: req.query.first + " " + req.query.last })
+})
 
+//#12
 
-
+app.post("/name", (req, res) => {
+  res.json({ name: req.body.first + " " + req.body.last })
+})
 
 
 
